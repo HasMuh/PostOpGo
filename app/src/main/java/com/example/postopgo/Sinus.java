38a3164ec05663c0f67c6f9caef43014b7b56b8e
HSNMUH <hsnmuh47@gmail.com>
2017-11-27 23:12:28 -0500

    Set up Sinus Surgery main page

--- app/src/main/java/com/example/postopgo/Sinus.java
+++ app/src/main/java/com/example/postopgo/Sinus.java
@@ -1,7 +1,13 @@
 package com.example.postopgo;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class Sinus extends AppCompatActivity {
 
@@ -9,5 +15,44 @@ public class Sinus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinus);
+    }
+    public void toFAQ(View view)
+    {
+        Intent intent = new Intent(this, FaqRhino.class);
+        startActivity(intent);
+    }
+
+    public void toDailyCare(View view)
+    {
+        Intent intent = new Intent(this, DCRhino.class);
+        startActivity(intent);
+    }
+
+    public void toContact(View view)
+    {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"borpas@njms.rutgers.com"});
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(emailIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+// Start an activity if it's safe
+        if (isIntentSafe)
+        {
+            startActivity(emailIntent);
+        }
+    }
+
+    public void toRestrictions(View view)
+    {
+        Intent intent = new Intent(this, RestRhino.class);
+        startActivity(intent);
+    }
+
+    public void toMeds(View view)
+    {
+        Intent intent = new Intent(this, RhinoMeds.class);
+        startActivity(intent);
     }
 }
